dae4c2dfd826c7892ef8f11dfa689a961a43b6c7
Gabriel Souza <gabriel@angularis.tech>
2021-02-01 22:50:00 -0300

    Added function to read csv data.

---
+++ tools/FileManager.go
@@ -0,0 +1,31 @@
+package tools
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func ReadData(fileName string) ([][]string, error) {
+
+	f, err := os.Open(fileName)
+
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	// skip first line
+	if _, err := r.Read(); err != nil {
+		return [][]string{}, err
+	}
+
+	records, err := r.ReadAll()
+
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return records, nil
+}
